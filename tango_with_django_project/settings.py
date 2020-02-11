8eb94800e79af110114e1e6411067e6783edb95d
chenyongzhuang <839017328@qq.com>
2020-02-11 04:51:56 +0000

    chapter3 , finished the exercise

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
@@ -120,12 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rango',
-]
+

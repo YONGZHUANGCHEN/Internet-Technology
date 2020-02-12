0f6e8127aed159722f6cb0ade446d9c24d2048ba
chenyongzhuang <839017328@qq.com>
2020-02-12 23:02:45 +0000

    chapter 7, finished the exercise.In chapter6,the path is wrong, and has changed

--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique= True)
+    MAX_NAME_LENGTH = 128
+    name = models.CharField(max_length=MAX_NAME_LENGTH, unique= True)
     views =models.IntegerField(default=0)
     likes =models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -11,14 +12,20 @@ class Category(models.Model):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.name
 
 
     class Meta:
         verbose_name_plural = 'Categories'
 
+    def __str__(self):
+        return self.name
+
+
 class Page(models.Model):
+
+     MAX_TITLE_LENGTH = 128
+     MAX_URL_LENGTH = 200
+
      category = models.ForeignKey(Category, on_delete=models.CASCADE)
      title = models.CharField(max_length=128)
      url = models.URLField()

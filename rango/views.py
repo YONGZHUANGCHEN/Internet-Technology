f122a725827b53e250f63e0e0d7ea914e28df999
chenyongzhuang <839017328@qq.com>
2020-02-16 02:49:20 +0000

    chapter10, the last commit was misnamed, the context has no changed

--- rango/views.py
+++ rango/views.py
@@ -11,7 +11,6 @@ from datetime import datetime
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
